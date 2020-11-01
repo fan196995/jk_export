c1f2c4a740e29c69680bb85ac138df20556c0efa
fan196995 <59138325+fan196995@users.noreply.github.com>
2020-11-01 22:05:08 +0800

    Update ExportVo.java

--- src/main/java/cn/itcast/export/vo/ExportVo.java
+++ src/main/java/cn/itcast/export/vo/ExportVo.java
@@ -10,6 +10,6 @@ import javax.xml.bind.annotation.XmlRootElement;
 import cn.itcast.export.domain.Export;
 @XmlRootElement(name="export")
 public class ExportVo extends Export {
-
+hahahahahah
 	
 }
